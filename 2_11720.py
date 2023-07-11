ccabfc751d27787e6ae16e9f145fc0ea91cd346e
jaeyeolee <jy.lee@teamsparta.co>
2023-07-11 22:40:13 +0900

    11720 숫자의 합

--- 2_11720.py
+++ 2_11720.py
@@ -3,8 +3,15 @@
 숫자: 11720
 유형:
 날짜: 2023-07-11
-난이도: 1 ~ 5
+난이도: 1
 메모:
 """
 
 N = int(input())
+NUM_STR = str(input())
+
+total = 0
+for i in range(len(NUM_STR)):
+    total += int(NUM_STR[i])
+
+print(total)
